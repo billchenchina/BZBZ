7af8519b6670803def11ac59d70a683fe0578e15
Billchenchina <billchenchina2001@gmail.com>
2018-05-07 14:47:08 +0800

    Feat: local cache(#5)

--- js/problem.js
+++ js/problem.js
@@ -18,29 +18,41 @@ appendLink = function(url, title) {
   ele[0].appendChild(link);
 };
 
-function add_link() {
-  var problem_id_q0dsah6q4v2ex6oh4 = location.href.split("=")[1];
-  /*
-    $.getJSON("./result.json", function (result) {
-        var result = result.sort();
-        vm.problems = result
-    });*/
-  $.getJSON("https://aker.club/bzojch/result.json", function(result) {
-    for (let i in result) {
-      if (result[i][0] == problem_id_q0dsah6q4v2ex6oh4) {
-        console.log(result[i]);
+_add_link = function(problem_id) {
+  var problem_id = location.href.split("=")[1];
+  chrome.storage.local.get(["bzoj_json"], function(result){
+    for (let i in result.bzoj_json) {
+      if (result.bzoj_json[i][0] == problem_id) {
+        console.log(result.bzoj_json[i]);
 
-        var res1 = result[i][4];
-        var res2 = result[i][6];
+        var res1 = result.bzoj_json[i][4];
+        var res2 = result.bzoj_json[i][6];
 
         console.log(ojLink(res1));
         console.log(ojLink(res2));
         const ele = document.getElementsByTagName("h2");
-        ele[0].innerText = "找到如下链接(点击进入):" + "\n";
+        ele[0].innerText += "\n找到如下链接(点击进入):" + "\n";
         appendLink(ojLink(res1), res1);
         appendLink(ojLink(res2), res2);
       }
     }
   });
+}
+
+function add_link() {
+  chrome.storage.local.get(["bzoj_json"], function(result){
+    if(result.bzoj_json == null) {
+      console.log("Data Not Found, Downloading");
+      $.getJSON("https://ruanx.pw/bzojch/result.json", function(result) {
+        chrome.storage.local.set({"bzoj_json": result}, _add_link);
+      });
+    } else {
+      console.log("Using local cache");
+      _add_link();
+    }
+  });
+
+
+
 }
 add_link();
